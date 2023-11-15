26ba56962fc339fabce15ac4293da0250c8f514c
hariprakash-max <148082831+hariprakash-max@users.noreply.github.com>
2023-11-15 18:57:57 +0530

    Add files via upload

---
+++ distance.py
@@ -0,0 +1,7 @@
+import math
+x1=float(input("enter x1 value:"))
+y1=float(input("enter y1 value:"))
+x2=float(input("enter x2 value:"))
+y2=float(input("enter y2 value:"))
+distance=((x1-x2)**2)+((y1-y2)**2)
+print("distance between two co-ordinates is%,2f%distance",distance)
---
+++ employee.py
@@ -0,0 +1,8 @@
+a=input("enter the name:")
+salary=23000
+if(salary<15000):
+    print("hr")
+elif(salary>10000):
+    print("software developer")
+else:
+    print("none")
