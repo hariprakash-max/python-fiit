011567b94ce517ee20c96627013a63c76a39a373
hariprakash-max <148082831+hariprakash-max@users.noreply.github.com>
2023-11-16 16:35:30 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,11 @@
+for i in range(0,50):
+    print(i)
+    if i == 25:
+        break
+
+# for loop using a to z
+
+for char in range(ord('a'),ord('z')+1):
+    print(chr(char),end="")
+for char in range(ord('a'),ord('z')+1):
+    print(char)
---
+++ while_loop.py
@@ -0,0 +1,4 @@
+a=100
+while a>50:
+    print(a)
+    a-=1
