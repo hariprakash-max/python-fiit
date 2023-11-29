a24e246130166843e248ab3dbec82814e03ca2f4
hariprakash-max <148082831+hariprakash-max@users.noreply.github.com>
2023-11-29 19:35:05 +0530

    Add files via upload

---
+++ math1.py
@@ -0,0 +1,7 @@
+from modules import add,subtract,multiply
+result_add=add(5,3)
+result_subtract=subtract(6,8)
+result_multiply=multiply(3,7)
+print(f"addition:{result_add}")
+print(f"subtract:{result_subtract}")
+print(f"multiply:{result_multiply}")
